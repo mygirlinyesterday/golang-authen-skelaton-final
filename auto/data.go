f9b1727498e6b4f5df92cd31c66b5092f5c36a46
mygirlinyesterday <mygirlinyesterday@gmail.com>
2019-06-02 16:39:41 +0700

    golang skelaton with authen v0.3

--- auto/data.go
+++ auto/data.go
@@ -3,9 +3,9 @@ package auto
 import "github.com/playground/api/models"
 
 var users = []models.User{
-	models.User{Nickname: "App", Email: "app@gmail.com", Password: "app1234"},
-	models.User{Nickname: "Joy", Email: "joy@gmail.com", Password: "Joy1234"},
-	models.User{Nickname: "Ning", Email: "ning@gmail.com", Password: "ning1234"},
-	models.User{Nickname: "May", Email: "may@gmail.com", Password: "may1234"},
-	models.User{Nickname: "Noey", Email: "noey@gmail.com", Password: "noey1234"},
+	models.User{Nickname: "Aaa", Email: "aaa@gmail.com", Password: "aaa1234"},
+	models.User{Nickname: "Bbb", Email: "bbb@gmail.com", Password: "bbb1234"},
+	models.User{Nickname: "Ccc", Email: "ccc@gmail.com", Password: "ccc1234"},
+	models.User{Nickname: "Ddd", Email: "ddd@gmail.com", Password: "ddd1234"},
+	models.User{Nickname: "Eee", Email: "Eee@gmail.com", Password: "eee1234"},
 }
