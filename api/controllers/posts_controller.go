744664a03c916f64420b8944bb7e51a2b3172cd1
mygirlinyesterday <mygirlinyesterday@gmail.com>
2019-06-05 01:04:41 +0700

    golang skelaton with authen v0.4

--- api/controllers/posts_controller.go
+++ api/controllers/posts_controller.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 
 	"github.com/playground/api/database"
 	"github.com/playground/api/models"
@@ -12,6 +15,26 @@ import (
 	"github.com/playground/api/repository/crud"
 	"github.com/playground/api/responses"
 )
+
+func GetPosts(w http.ResponseWriter, r *http.Request) {
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	repo := crud.NewRepositoryPostsCRUD(db)
+
+	func(postsRepository repository.PostRepository) {
+		posts, err := postsRepository.FindAll()
+		if err != nil {
+			responses.ERROR(w, http.StatusInternalServerError, err)
+			return
+		}
+		responses.JSON(w, http.StatusOK, posts)
+	}(repo)
+}
 
 func CreatePost(w http.ResponseWriter, r *http.Request) {
 
@@ -30,6 +53,10 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 
 	post.Prepare()
 	err = post.Validate()
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
 
 	db, err := database.Connect()
 	if err != nil {
@@ -47,5 +74,107 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 		}
 		w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.RequestURI, post.ID))
 		responses.JSON(w, http.StatusCreated, post)
+	}(repo)
+}
+
+func GetPost(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	pid, err := strconv.ParseUint(vars["id"], 10, 64)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	repo := crud.NewRepositoryPostsCRUD(db)
+
+	func(postsRepository repository.PostRepository) {
+		post, err := postsRepository.FindById(pid)
+		if err != nil {
+			responses.ERROR(w, http.StatusBadRequest, err)
+			return
+		}
+		responses.JSON(w, http.StatusOK, post)
+	}(repo)
+}
+
+func UpdatePost(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	pid, err := strconv.ParseUint(vars["id"], 10, 64)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	post := models.Post{}
+	err = json.Unmarshal(body, &post)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	post.Prepare()
+	err = post.Validate()
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	repo := crud.NewRepositoryPostsCRUD(db)
+
+	func(postsRepository repository.PostRepository) {
+		rows, err := postsRepository.Update(pid, post)
+		if err != nil {
+			responses.ERROR(w, http.StatusBadRequest, err)
+			return
+		}
+		responses.JSON(w, http.StatusOK, rows)
+	}(repo)
+}
+
+func DeletePost(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	pid, err := strconv.ParseUint(vars["id"], 10, 64)
+	if err != nil {
+		responses.ERROR(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	repo := crud.NewRepositoryPostsCRUD(db)
+
+	func(postsRepository repository.PostRepository) {
+		_, err := postsRepository.Delete(pid)
+		if err != nil {
+			responses.ERROR(w, http.StatusBadRequest, err)
+			return
+		}
+		w.Header().Set("Enity", fmt.Sprintf("%d", pid))
+		responses.JSON(w, http.StatusNoContent, "")
 	}(repo)
 }
--- api/router/routes/posts_routes.go
+++ api/router/routes/posts_routes.go
@@ -9,7 +9,27 @@ import (
 var postsRoutes = []Route{
 	Route{
 		Uri:     "/posts",
+		Method:  http.MethodGet,
+		Handler: controllers.GetPosts,
+	},
+	Route{
+		Uri:     "/posts",
 		Method:  http.MethodPost,
 		Handler: controllers.CreatePost,
+	},
+	Route{
+		Uri:     "/posts/{id}",
+		Method:  http.MethodGet,
+		Handler: controllers.GetPost,
+	},
+	Route{
+		Uri:     "/posts/{id}",
+		Method:  http.MethodPut,
+		Handler: controllers.UpdatePost,
+	},
+	Route{
+		Uri:     "/posts/{id}",
+		Method:  http.MethodDelete,
+		Handler: controllers.DeletePost,
 	},
 }
